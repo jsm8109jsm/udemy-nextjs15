2c5782b199daf510a720d346d5729991ed10d39d
정 승민 <jsm8109jsm@gmail.com>
2024-11-21 14:05:42 +0000

    108~114

--- foodies-starting-project/app/meals/page.js
+++ foodies-starting-project/app/meals/page.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+
+async function Meals() {
+  const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
 
 function MealsPage() {
   return (
@@ -10,17 +17,20 @@ function MealsPage() {
         <h1>
           Delicious meals, created{" "}
           <span className={classes.highlight}>by you</span>
-          <p>
-            Choose your favorite recipe and cook it yourself. It is easy and
-            fun!
-          </p>
-          <p className={classes.cta}>
-            <Link href="/meals/share">Share Your Favorite Recipe</Link>
-          </p>
         </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
--- foodies-starting-project/components/meals/meals-grid.js
+++ foodies-starting-project/components/meals/meals-grid.js
@@ -1,4 +1,5 @@
 import classess from "./meals-grid.module.css";
+import MealsItem from "./meal-item";
 
 export default function MealsGrid({ meals }) {
   return (
