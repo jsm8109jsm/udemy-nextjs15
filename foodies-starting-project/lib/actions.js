a7f5a0f2d8b2928282b3f0c9e302f8d8c405b86e
정 승민 <jsm8109jsm@gmail.com>
2024-11-24 10:51:35 +0000

    126~134

--- foodies-starting-project/lib/actions.js
+++ foodies-starting-project/lib/actions.js
@@ -2,12 +2,13 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
 function isInvalidText(text) {
   return !text || text.trim() === "";
 }
 
-export async function shareMeal(formData) {
+export async function shareMeal(prevState, formData) {
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -27,8 +28,11 @@ export async function shareMeal(formData) {
     !meal.image ||
     meal.image.size === 0
   ) {
-    throw new Error("Invalid input");
+    return {
+      message: "Invalid input.",
+    };
   }
   await saveMeal(meal);
+  revalidatePath("/meals");
   redirect("/meals");
 }
