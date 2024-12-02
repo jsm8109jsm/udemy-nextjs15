38b85226b7aa6e301532070cfbdedfae82768823
정 승민 <jsm8109jsm@gmail.com>
2024-12-02 14:26:56 +0000

    176~181

--- 5-starting-project/actions/posts.js
+++ 5-starting-project/actions/posts.js
@@ -1,5 +1,7 @@
 "use server";
-import { storePost } from "@/lib/posts";
+import { uploadImage } from "@/lib/cloudinary";
+import { storePost, updatePostLikeStatus } from "@/lib/posts";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function createPost(prevState, formData) {
@@ -25,12 +27,27 @@ export async function createPost(prevState, formData) {
     return { errors };
   }
 
+  let imageUrl;
+
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error(
+      "Image upload failted, post was not created. Please try again later."
+    );
+  }
+
   await storePost({
-    imageUrl: "",
+    imageUrl,
     title,
     content,
     userId: 1,
   });
-
+  revalidatePath("/", "layout");
   redirect("/feed");
+}
+
+export async function togglePostLikeStatus(postId) {
+  updatePostLikeStatus(postId, 2);
+  revalidatePath("/", "layout");
 }
